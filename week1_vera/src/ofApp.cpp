e695f47820f1d845a1d8a1df7a4a5f0cf06b96e3
cathy-mengying-fang <fangcat16@icloud.com>
2022-02-08 22:38:57 -0500

    random saw lines

--- week1_vera/src/ofApp.cpp
+++ week1_vera/src/ofApp.cpp
@@ -12,6 +12,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    ofSeedRandom(mouseX);
     ofBackground(239,237,229);
     ofSetColor(92,111,133);
     ofSetLineWidth(0.5);
@@ -20,13 +21,19 @@ void ofApp::draw(){
         int rowHeight = 75 + row * gap;
         int startX = 100;
         // endX = 700
+        float prevX = startX;
         for (int seg = 0; seg < 40; seg ++){
-            int y = rowHeight + 10;
-            int y1 = rowHeight - 10;
-            int x = startX + seg * 15;
-            ofDrawLine(x, y, x+7.5, y1);
-            int y2 = rowHeight + 10;
-            ofDrawLine(x+7.5, y1, x+15, y2);
+//            float yNoise = 10 + ofRandom(0,1)*2;
+            float y = rowHeight + ofRandom(0,1)*10;
+            float y1 = rowHeight - ofRandom(0,1)*10;
+            float xNoise = ofRandom(25,30);
+            float midX = prevX + xNoise;
+            ofDrawLine(prevX, y, midX, y1);
+            
+            float x2 = ofRandom(prevX,midX);
+            float y2 = rowHeight + ofRandom(0,1)*10;
+            ofDrawLine(midX, y1, x2, y2);
+            prevX = x2;
         }
     }
 
