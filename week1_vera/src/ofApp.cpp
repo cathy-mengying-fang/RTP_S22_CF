1b5a538151070f7d97493a1afa5f489d4d26eab6
cathy-mengying-fang <fangcat16@icloud.com>
2022-02-08 22:10:39 -0500

    ignore gen img; saw lines

--- week1_vera/src/ofApp.cpp
+++ week1_vera/src/ofApp.cpp
@@ -15,16 +15,32 @@ void ofApp::draw(){
     ofBackground(239,237,229);
     ofSetColor(92,111,133);
     ofSetLineWidth(0.5);
-    for (int i = 0; i < 24; i++){
+    for (int row = 0; row < 24; row ++){
         int gap = 27.5;
-        int y = 75 + i*gap;
-        ofDrawLine(100, y, 700, y);
+        int rowHeight = 75 + row * gap;
+        int startX = 100;
+        // endX = 700
+        for (int seg = 0; seg < 40; seg ++){
+            int y = rowHeight + 10;
+            int y1 = rowHeight - 10;
+            int x = startX + seg * 15;
+            ofDrawLine(x, y, x+7.5, y1);
+            int y2 = rowHeight + 10;
+            ofDrawLine(x+7.5, y1, x+15, y2);
+        }
     }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+    if (key == 's'){
+        ofImage image;
+        int width = ofGetWindowWidth();
+        int height = ofGetWindowHeight();
+        image.grabScreen(0, 0, width, height);
+        ofSaveImage(image,"file.jpg"); //save to disk
+    }
 
 }
 
