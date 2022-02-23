4a0075a505812b2c779db4fc66f3951a4332787e
cathy-mengying-fang <fangcat16@icloud.com>
2022-02-22 23:12:10 -0500

    blend mode; recreate path from smoothed polyline and added fill

--- week3_cooper/src/ofApp.cpp
+++ week3_cooper/src/ofApp.cpp
@@ -13,20 +13,50 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
     
-    ofBackground(194,176,159);
+//    ofBackground(194,176,159);
+    ofBackground(255,255,255,255);
     ofColor red = ofColor(163,65,55);
     ofColor yellow = ofColor(195,159,54);
     ofColor blue = ofColor(17,97,118);
     ofColor colors [3] = {red,yellow,blue};
     
     vector < ofPath > paths = font.getStringAsPoints("5");
-    for (int j=0; j<7; j++){
+    for (int num=0; num<7; num++){
         ofPushMatrix();
-        ofTranslate(400+120*j, 550);
-        ofSetColor(colors[j%3]);
+        ofTranslate(400+120*num, 550);
         for (int i=0; i<paths.size(); i++){
-            paths[i].draw();
+            ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
+//            paths[i].draw();
+            
+            paths[i].setPolyWindingMode(OF_POLY_WINDING_ODD);
+            vector < ofPolyline > lines = paths[i].getOutline();
+            for (int j=0; j<lines.size();j++){
+//                ofEnableSmoothing();
+                lines[j].setClosed(true);
+                lines[j] = lines[j].getResampledBySpacing(3);
+                lines[j] = lines[j].getSmoothed(10);
+                ofPolyline smoothedPolyline = lines[j];
+                ofPath smoothedPath;
+//                https://forum.openframeworks.cc/t/fill-polyline/10800/2
+                for( int k = 0; k < smoothedPolyline.getVertices().size(); k++) {
+                        if(k == 0) {
+                            smoothedPath.newSubPath();
+                            smoothedPath.moveTo(smoothedPolyline.getVertices()[k] );
+                        } else {
+                            smoothedPath.lineTo(smoothedPolyline.getVertices()[k] );
+                        }
+                    }
+                smoothedPath.close();
+                smoothedPath.setColor(colors[num%3]);
+                smoothedPath.draw();
+                
+//                lines[j].draw();
+                
+            }
+            ofDisableBlendMode();
+           
         }
+        
         ofPopMatrix();
     }
 }
