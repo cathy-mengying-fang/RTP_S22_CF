7a94c5923a65802efc1a31011a6ed00d2a168c51
cathy-mengying-fang <fangcat16@icloud.com>
2022-03-02 01:30:47 -0500

    final

--- week4_riley/src/ofApp.cpp
+++ week4_riley/src/ofApp.cpp
@@ -12,22 +12,83 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackground (255);
-    
+    ofBackground (0);
+    int xInc;
+    int yInc;
+    int x = 400;
     int y = 400;
-    for (int i=0; i<8; i++){
-        int x = 400;
-        int r = 200;
-        if (i%2 == 1){
-            ofSetColor(0);
+    int r = 500;
+    int prevX = x;
+    int prevY = y;
+    int prevR = r;
+    for (int j = 0; j<3; j++){
+        x = prevX;
+        y = prevY;
+        r = prevR;
+        if (j==0){
+            xInc = 20;
+            yInc = 20;
         }
-        else{
-            ofSetColor(255);
+        else if (j==1){
+            xInc = 20;
+            yInc = -20;
         }
-        ofDrawCircle(x-10*i,y,r-20*i);
+        else {
+            xInc = -20;
+            yInc = 0;
+        }
+        for (int i = 0; i<4; i++){
+            x = x+xInc;
+            y = y+yInc;
+//            float pct = ofMap(j,1,4,1,0);
+            float pct = powf((4-j), 0.8);
+            r = r-15*pct;
+//            ofSetColor(255,0,0);
+//            ofDrawCircle(x,y,2);
+            if (i%2 == 0){
+                ofSetColor(255);
+            }
+            else{
+                ofSetColor(0);
+            }
+//            ofNoFill();
+//            ofSetColor(255);
+            ofSetCircleResolution(100);
+            ofDrawCircle(x,y,r);
+        }
+        prevX = x;
+        prevY = y;
+        prevR = r;
     }
     
-
+    
+    
+    
+    
+    
+    
+    
+    
+////    for (int j=0;j<3;j++){
+////        if (j==0){
+//            int y = 400;
+//            for (int i=0; i<8; i++){
+//                int x = 400;
+//                int r = 20;
+//                if (i%2 == 0){
+//                    ofSetColor(255);
+//                }
+//                else{
+//                    ofSetColor(0);
+//                }
+////                ofNoFill();
+//                ofDrawCircle(x+10*i,y,r+20*i);
+////            }
+////        }
+////        if (j==1){
+////
+////        }
+//    }
 }
 
 //--------------------------------------------------------------
