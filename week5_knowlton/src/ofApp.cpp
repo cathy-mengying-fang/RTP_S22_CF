41eed661765c567e54afad5c5c7430913e48b246
cathy-mengying-fang <fangcat16@icloud.com>
2022-03-08 23:42:39 -0500

    cursor blend

--- week5_knowlton/src/ofApp.cpp
+++ week5_knowlton/src/ofApp.cpp
@@ -13,32 +13,30 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
     ofColor color;
-    int window = 5;
+//    int window = 100;
+//    int sumR;
+//    int sumG;
+//    int sumB;
+//    float pct = ofMap(mouseX, 0, 1200, 0, 1);
     for (int i = 0; i < 400; i++){
-        int sumR = 0;
-        int sumG = 0;
-        int sumB = 0;
-        int avgR = 0;
-        int avgG = 0;
-        int avgB = 0;
-        for (int j = 0; j <= 800-window; j++){
-            sumR = 0;
-            sumG = 0;
-            sumB = 0;
-            avgR = 0;
-            avgG = 0;
-            avgB = 0;
-            for (int k = j; k < j + window; k++){
-                sumR += davinci.getColor(i, j).r;
-                sumG += davinci.getColor(i, j).g;
-                sumB += davinci.getColor(i, j).b;
-                sumR += monalisa.getColor(i, j).r;
-                sumG += monalisa.getColor(i, j).g;
-                sumB += monalisa.getColor(i, j).b;
+//        sumR = 0;
+//        sumG = 0;
+//        sumB = 0;
+        for (int j = 0; j < 800; j++){
+            
+            float distance = ofDist(mouseX, mouseY, i, j);
+            if (distance < 100){
+                float pct = ofMap(distance, 0, 100, 1,0);
+                monaleo.setColor(i,j, ofColor(davinci.getColor(i,j)*pct + monalisa.getColor(i,j)*(1-pct)));
+
+            } else {
+                monaleo.setColor(i,j, ofColor(monalisa.getColor(i, j)));
+
             }
-            avgR = int(sumR/window);
-            avgG = int(sumG/window);
-            avgB = int(sumB/window);
+                        
+//            sumR = sumR + (davinci.getColor(i, j).r*pct + monalisa.getColor(i, j).r*(1-pct));
+//            sumG = sumG + (davinci.getColor(i, j).g*pct + monalisa.getColor(i, j).g*(1-pct));
+//            sumB = sumB + (davinci.getColor(i, j).b*pct + monalisa.getColor(i, j).b*(1-pct));
             
 //            if (int(j/10)%2 ==0){
 //                color = davinci.getColor(i, j);
@@ -46,7 +44,7 @@ void ofApp::update(){
 //            else if(int(j/10)%2 ==1){
 //                color = monalisa.getColor(i, j);
 //            }
-            monaleo.setColor(i,j,ofColor(avgR,avgG,avgB));
+//            monaleo.setColor(i,j,ofColor(sumR,sumG,sumB));
         }
     }
     monaleo.update();
